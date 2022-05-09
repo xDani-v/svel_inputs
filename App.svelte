1b0ca2b49f1b32cd7b623393162c1d0b15e02a63
Daniel Villano <84256368+xDani-v@users.noreply.github.com>
2022-05-09 13:46:14 -0500

    Add files via upload

---
+++ App.svelte
@@ -0,0 +1,30 @@
+<script>
+	let rango = "";
+	let check = false;
+	let frutas = ['limon','frutilla','tomate'];
+	let array = []
+	let radio = "";
+	$: console.log(array);
+</script>
+
+<div>
+	{#each frutas as fruta}
+		 <input type="checkbox" value={fruta} bind:group={array}>
+		 {fruta}
+	{/each}
+	<p>fruta:{array}</p>
+
+	<!-- {#each frutas as fruta}
+		 <input type="radio" value={fruta} bind:group={radio}>
+		 {fruta}
+	{/each}
+	<p>fruta:{radio}</p> -->
+
+
+	<!-- <h1>Buildings</h1>
+
+	<input type="checkbox" bind:checked={check}>
+	<p>{check}</p> -->
+	<!-- <input type="range" bind:value={rango} min="0" max="100">
+	<p>texto:{rango}</p> -->
+</div>
